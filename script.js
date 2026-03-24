3e0da45094de2a4d033c9570af296d8e7ff11a7f
Dione Schiefferdecker <dionerafael007@gmail.com>
2026-03-24 14:50:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+feather.replace();
+
+const menuToggle = document.getElementById("menuToggle");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuToggle.addEventListener("click", () => {
+  mobileMenu.classList.toggle("show");
+});
